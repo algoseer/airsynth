77addd206ad4461a019ac4b6224a92b509799af4
algoseer <knaveen87@gmail.com>
2020-05-25 00:26:45 -0700

    added amplitude to modulation params

--- player.py
+++ player.py
@@ -28,9 +28,9 @@ class Player:
 		while not self.done:
 			data = self.pos_queue.get()
 			self.pos_queue.queue.clear()
-			note = self.pos_to_note_custom(data)
-			if note:
-				self.note_buffer = self.synth_note(note)
+			note_params = self.pos_to_note_custom(data, scalename='blues_6M')
+			if note_params['pitch']:
+				self.note_buffer = self.synth_note(note_params)
 			else:
 				self.note_buffer = None
 
@@ -43,14 +43,14 @@ class Player:
 	def pos_to_note_custom(self, x, scalename = 'pentatonic'):
 		#Rule for mapping absolute position to a note information
 		min_x = 0
-		max_x = 5
+		max_x = 6
 		base_note = 220 #A0
 		num_octaves = 4
 
 		if scalename == 'pentatonic':
 			scale=[0,3,5,7,10]
-		elif scalename == 'blues_6':
-			scale=[0,2,3,4,7,9]
+		elif scalename == 'blues_6M':
+			scale=[0,3,5,6,7,10]
 			
 
 		exp_scale=[]
@@ -63,9 +63,11 @@ class Player:
 			note_idx = (x[0] - min_x) * num_octaves * len(scale) // max_x
 			note = exp_scale[int(note_idx)]
 			pitch = base_note * 2 ** (note/12)
-			return pitch
+			amp = (x[1]-1)/3.
+			params = {'pitch':pitch, 'amp':amp}
+			return params
 		else:
-			return 0
+			return {'pitch':0,'amp':0}
 			
 	def pos_to_note(self, x):
 		#Rule for mapping absolute position to a note information
@@ -79,12 +81,16 @@ class Player:
 		if x[1] > 1:
 			note = (x[0] - min_x) * num_octaves * 12 // max_x
 			pitch = base_note * 2 ** (note/12)
-			return pitch
+			amp = (x[1]-1)/3.
+			params = {'pitch':pitch, 'amp':amp}
+			return params
 		else:
-			return 0
+			return {'pitch':0,'amp':0}
 
-	def synth_note(self, pitch, dur = None):
+	def synth_note(self, params, dur = None):
 		
+		pitch = params['pitch']
+		amp = params['amp']
 		if dur is None:
 			dur = self.dur
 		
@@ -93,7 +99,7 @@ class Player:
 		t = np.linspace(0, dur, int(dur * SAMPLE_RATE))
 		sig = np.sin(2* np.pi * t * pitch + self.phase)
 		self.phase += dur *2*np.pi * pitch
-		sig = 0.8 *sig.astype(np.float32)
+		sig = 0.8 *amp *sig.astype(np.float32)
 		
 		return sig
 
